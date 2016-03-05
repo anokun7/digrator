44ce42a5d5a84a53d0ecdd367444ad3c303bdfba
Anoop Kumar <hi2anoop@gmail.com>
2016-03-05 06:17:25 +0000

    Update to create repos successfully

--- migrate.sh
+++ migrate.sh
@@ -13,7 +13,7 @@ getAllRepos() {
   curl -s --user \
   	"$SOURCE_DTR_ADMIN":"$SOURCE_DTR_PASSWORD" --insecure \
   	https://"$SOURCE_DTR_DOMAIN"/api/v0/repositories | \
-  		jq '.repositories[] | {  (.namespace):(.name) }' | grep -v '[{}]' | sed -e 's/:\s*/\//' -e 's/"//g' 
+  		jq '.repositories[] | {  (.namespace):(.name) }' | grep -v '[{}]' | sed -e 's/:\s*/\//' -e 's/\s*"\s*//g' 
 }
 
 getTagsPerRepo() {
@@ -36,21 +36,38 @@ createNameSpaces() {
     do
       sed 's/^\s*"\(.*\)":\s*"\(.*\)"/{"type": "\1", "name": "\2" }/' /var/tmp/accounts > /var/tmp/dest_accounts
     done
-  IFS=
+
   cat /var/tmp/dest_accounts | sort -u | while IFS= read -r i;
     do
-      curl --insecure --user "$DEST_DTR_ADMIN":"$DEST_DTR_PASSWORD" -X POST --header "Content-Type: application/json" --header "Accept: application/json" --header "X-Csrf-Token: lCag0CgWAlVzYuTNCinQbbDYqvfo2b6-W1zpvyY52S0="  -d "$i" https://"$DEST_DTR_DOMAIN"/api/v0/accounts
+      curl --insecure --user "$DEST_DTR_ADMIN":"$DEST_DTR_PASSWORD" -X POST --header "Content-Type: application/json" \
+         --header "Accept: application/json" --header "X-Csrf-Token: lCag00CgWAlVzYuTNCinQbbDYqvfo2b6-W1zpvyY52S0=" \
+         -d "$i" https://"$DEST_DTR_DOMAIN"/api/v0/accounts
     done
 }
       
 getAllTags() {
-  for i in `getAllRepos`
+  cat /var/tmp/repos | sort -u | while IFS= read -r i;
     do
       getTagsPerRepo $i
     done
 }
 
-getAllRepos
+createRepos() {
+  cat /var/tmp/repos | sort -u | while IFS= read -r i;
+    do
+      sed 's^/\(.*\)$^/{"name": "\1", "visibility": "public" }^' /var/tmp/repos > /var/tmp/dest_repos
+    done
+
+  cat /var/tmp/dest_repos | sort -u | while IFS= read -r i;
+    do
+      curl --insecure --user "$DEST_DTR_ADMIN":"$DEST_DTR_PASSWORD" -X POST --header "Content-Type: application/json" \
+        --header "Accept: application/json" --header "X-Csrf-Token: jaYLzzlpH1SB0uv217SExOfwK8t-7QdXA6PymflfOOs=" \
+        -d "`echo $i | awk -F/ '{ print $2 }'`" "https://"$DEST_DTR_DOMAIN"/api/v0/repositories/`echo $i | awk -F/ '{ print $1 }'`"
+    done
+}
+
+getAllRepos > /var/tmp/repos
 pullImages
 getAllAccounts > /var/tmp/accounts
 createNameSpaces
+createRepos
